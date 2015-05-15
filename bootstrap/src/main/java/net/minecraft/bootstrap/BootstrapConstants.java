ddc5ccad8749b61e48c6482e5cf38b821216298c
Emiliyan Kurtseliyanski <emiliyan9604bg@gmail.com>
2015-05-16 00:19:19 +0300

    Final fixes.

--- bootstrap/src/main/java/net/minecraft/bootstrap/BootstrapConstants.java
+++ bootstrap/src/main/java/net/minecraft/bootstrap/BootstrapConstants.java
@@ -1,8 +1,8 @@
 package net.minecraft.bootstrap;
 
 public class BootstrapConstants {
-    public static final String MD5_FILE = ""; // site.com/launcher/md5.txt
-    public static final String SERVER_NAME = ""; 
-    public static final String APPLICATION_NAME = "";
-    public static final String LAUNCHER_URL = ""; // site.com/launcher/launcher.jar
+    public static final String MD5_FILE = "http://hysteria-gaming.org/launcher/md5.txt"; // site.com/launcher/md5.txt
+    public static final String SERVER_NAME = "HysteriaCraft"; 
+    public static final String APPLICATION_NAME = "HysteriaCraft";
+    public static final String LAUNCHER_URL = "http://hysteria-gaming.org/launcher/launcher.jar"; // site.com/launcher/launcher.jar
 }
