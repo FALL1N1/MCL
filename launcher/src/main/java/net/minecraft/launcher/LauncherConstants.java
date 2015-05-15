bd0f6b92d54237ef5e64bbbc814db64275d10e7d
Emiliyan Kurtseliyanski <emiliyan9604bg@gmail.com>
2015-05-16 00:05:54 +0300

    Typo; Fixed build.

--- launcher/src/main/java/net/minecraft/launcher/LauncherConstants.java
+++ launcher/src/main/java/net/minecraft/launcher/LauncherConstants.java
@@ -16,7 +16,7 @@ public class LauncherConstants {
     public static final String LIBRARY_DOWNLOAD_BASE = "http://hysteria-gaming.org/launcher/resource/"; // http://site.com/ressource/
     public static final String URL_BLOG = "Page Accueil OU Tumblr";
     public static final String URL_STATUS_CHECKER = "http://status.mojang.com/check";
-    public static final String URL_BOOTSTRAP_DOWNLOAD = http://hysteria-gaming.org/launcher/bootstrap.jar""; //http://site.com/launcher/bootstrap.jar
+    public static final String URL_BOOTSTRAP_DOWNLOAD = "http://hysteria-gaming.org/launcher/bootstrap.jar"; //http://site.com/launcher/bootstrap.jar
     public static final URI URL_FORGOT_USERNAME = constantURI("http://help.mojang.com/customer/portal/articles/1233873");
     public static final URI URL_FORGOT_PASSWORD_MINECRAFT = constantURI("http://help.mojang.com/customer/portal/articles/329524-change-or-forgot-password");
     public static final URI URL_FORGOT_MIGRATED_EMAIL = constantURI("http://help.mojang.com/customer/portal/articles/1205055-minecraft-launcher-error---migrated-account");
